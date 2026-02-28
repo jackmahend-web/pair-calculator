ffd4cb0f0e24835a261653763e005daeb0bc62e7
jackmahend-web <jackmahend@gmail.com>
2026-02-28 05:56:44 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const cacheName = 'pair-calc-v1';
+const assets = ['./', './index.html'];
+
+// Install service worker and cache files
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
